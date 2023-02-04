fbe452eac326c3662325dc1474004a32f57d69f6
c210214 <92442535+C210214@users.noreply.github.com>
2023-02-04 19:42:05 +0900

    upload file

--- javascript/shop.js
+++ javascript/shop.js
@@ -18,3 +18,31 @@ $(document).ready(function(){
         });
     });
     });
+
+    $('input.input-qty').each(function() {
+        var $this = $(this),
+          qty = $this.parent().find('.is-form'),
+          min = Number($this.attr('min')),
+          max = Number($this.attr('max'))
+        if (min == 0) {
+          var d = 0
+        } else d = min
+        $(qty).on('click', function() {
+          if ($(this).hasClass('minus')) {
+            if (d > min) d += -1
+          } else if ($(this).hasClass('plus')) {
+            var x = Number($this.val()) + 1
+            if (x <= max) d += 1
+          }
+          $this.attr('value', d).val(d)
+        })
+      })
+
+      const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+
+function changeImage(e) {
+  activeImage.src = e.target.src;
+}
+productImages.forEach(image => image.addEventListener("click", changeImage));
+navItem.addEventListener('click', toggleNavigation);
